a96c8b123a2af411800592b0eba84ef022cf2b4f
Rodrigo Salinas <rodrigo@tivelabs.com>
2022-10-20 16:32:10 -0400

    my first commit

---
+++ src/callback/index.js
@@ -0,0 +1,21 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2);
+}
+
+console.log(calc(2, 2, sum));
+
+setTimeout(function () {
+    console.log('Hola Javascript');
+}, 5000);
+
+function greeting(name) {
+    console.log(`Hola ${name}`);
+}
+
+setTimeout(greeting, 2000, 'Nataly');
+
+
