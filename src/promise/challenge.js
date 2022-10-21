9ac52ea875227ca36e427f06f609a639ecf335f5
Rodrigo Salinas <rodrigo@tivelabs.com>
2022-10-21 15:19:05 -0400

    mi segundo curso

---
+++ src/promise/challenge.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch';
+const API = 'https://api.escuelajs.co/api/v1';
+
+function fetchData(urlApi) {
+    return fetch(urlApi);
+};
+
+// fetchData(`${API}/products`)
+//     .then(response => response.json())
+//     .then(products => {
+//         console.log(products);
+//     })
+//     .then(() => {
+//         console.log('hola');
+//     })
+//     .catch(error => console.log(error));
+
+fetchData(`${API}/products`)
+    .then(response => response.json())
+    .then(products => {
+        console.log(`MIS PRODUCTOS: ${products}`);
+        return fetchData(`${API}/products/${products[0].id}`)
+    })
+    .then(response => response.json())
+    .then(product => {
+        console.log(`PRODUCTO: ${product}`);
+        return fetchData(`${API}/categories/${product.category.id}`)
+    })
+    .then(response => response.json())
+    .then(category => {
+        console.log(`MIS categories: ${category}`);
+        console.log(category.name);
+    })
+    .catch(error => console.log(error))
+    .finally(() => console.log('finally'))
